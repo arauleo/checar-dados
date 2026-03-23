c3a2d6d197f9804dbc931636be488c4b423d1ba7
arauleo <leo.araujo1234@gmail.com>
2026-03-22 21:15:18 -0300

    Ãalteracao no main

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ola seja bem vindo! Como voce se chama?");
+        System.out.println("Olá seja bem vindo! Como voce se chama?");
         String nome = scanner.nextLine();
 
-        System.out.println("Digite por favor seu CPF com pontos e hifen:");
+        System.out.println("Digite por favor seu CPF com pontos e hífen:");
         String cpf = scanner.nextLine();
 
         System.out.println("E sobre seu estado civil?");
@@ -19,14 +19,14 @@ public class Main {
         System.out.println("Agora por favor sua altura:");
         double altura = scanner.nextDouble();
 
-        System.out.println("Quanto voce pesa?");
+        System.out.println("Quanto você pesa? (somente números)");
         double peso = scanner.nextDouble();
 
 
 
         scanner.close();
 
-        System.out.println("Vamos checar seus dados, " + nome + ", seu CPF: " + cpf + ", esta certo? Voce esta realmente " + civil + "?");
+        System.out.println("Vamos checar seus dados, " + nome + ", você tem " +idade+" anos? Seu CPF: " + cpf + ", está certo? Voce está realmente " + civil + "? Pesa " + peso + "kgs e tem " +altura+ " de altura?");
 
     }
 }
